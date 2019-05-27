4f0e2bfdcd143ef9939187fc177616bcd95fa490
webeweb <support@webeweb.fr>
2019-05-27 20:12:44 +0200

    Add Get video request

---
+++ src/Model/Request/GetVideoRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Model\Request;
+
+use WBW\Library\Pexels\Model\AbstractRequest;
+
+/**
+ * Get video request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Model\Request
+ */
+class GetVideoRequest extends AbstractRequest {
+
+    /**
+     * Get video resource path.
+     *
+     * @var string
+     */
+    const GET_VIDEO_RESOURCE_PATH = "/v1/videos/videos/:id";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcePath() {
+        return self::GET_VIDEO_RESOURCE_PATH;
+    }
+}
---
+++ tests/Model/Request/GetVideoRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Tests\Model\Request;
+
+use WBW\Library\Pexels\Model\Request\GetVideoRequest;
+use WBW\Library\Pexels\Tests\AbstractTestCase;
+
+/**
+ * Get video request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Tests\Model\Request
+ */
+class GetVideoRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("/v1/videos/videos/:id", GetVideoRequest::GET_VIDEO_RESOURCE_PATH);
+
+        $obj = new GetVideoRequest();
+
+        $this->assertEquals(GetVideoRequest::GET_VIDEO_RESOURCE_PATH, $obj->getResourcePath());
+    }
+}
