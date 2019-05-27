8e6a8d6a8e2337e5fca8e7c0edaff96671639cdb
webeweb <support@webeweb.fr>
2019-05-27 20:44:37 +0200

    Add Substitute request interface

---
+++ src/API/SubstituteRequestInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\API;
+
+/**
+ * Substitute request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\API
+ */
+interface SubstituteRequestInterface {
+
+    /**
+     * Get the substitute name.
+     *
+     * @return string Returns the substitute name.
+     */
+    public function getSubstituteName();
+
+    /**
+     * Get the substitute value.
+     *
+     * @return string Returns the substitute value.
+     */
+    public function getSubstituteValue();
+}
