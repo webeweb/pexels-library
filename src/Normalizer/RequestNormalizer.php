0d7e2257f0206c6a2235f503a561b98584bc708f
webeweb <support@webeweb.fr>
2019-05-27 22:12:01 +0200

    Add Request normalizer

---
+++ src/Normalizer/RequestNormalizer.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Normalizer;
+
+use InvalidArgumentException;
+use WBW\Library\Core\Argument\ArrayHelper;
+use WBW\Library\Pexels\Model\Request\CuratedPhotosRequest;
+use WBW\Library\Pexels\Model\Request\PopularVideosRequest;
+use WBW\Library\Pexels\Model\Request\SearchPhotosRequest;
+use WBW\Library\Pexels\Model\Request\SearchVideosRequest;
+
+/**
+ * Request normalizer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Normalizer
+ */
+class RequestNormalizer {
+
+    /**
+     * Normalize a curated photos request.
+     *
+     * @param CuratedPhotosRequest $request The curated photos request.
+     * @return array Returns the parameters.
+     */
+    public static function normalizeCuratedPhotosRequest(CuratedPhotosRequest $request) {
+
+        $parameters = [];
+
+        ArrayHelper::set($parameters, "per_page", $request->getPerPage(), [null]);
+        ArrayHelper::set($parameters, "page", $request->getPage(), [null]);
+
+        return $parameters;
+    }
+
+    /**
+     * Normalize a popular videos request.
+     *
+     * @param PopularVideosRequest $request The popular photos request.
+     * @return array Returns the parameters.
+     */
+    public static function normalizePopularVideosRequest(PopularVideosRequest $request) {
+
+        $parameters = [];
+
+        ArrayHelper::set($parameters, "per_page", $request->getPerPage(), [null]);
+        ArrayHelper::set($parameters, "page", $request->getPage(), [null]);
+        ArrayHelper::set($parameters, "min_width", $request->getMinWidth(), [null]);
+        ArrayHelper::set($parameters, "max_width", $request->getMaxWidth(), [null]);
+        ArrayHelper::set($parameters, "min_duration", $request->getMinDuration(), [null]);
+        ArrayHelper::set($parameters, "max_duration", $request->getMaxDuration(), [null]);
+
+        return $parameters;
+    }
+
+    /**
+     * Normalize a search photos request.
+     *
+     * @param SearchPhotosRequest $request The search photos request.
+     * @return array Returns the parameters.
+     * @throws InvalidArgumentException Throws an invalid argument exception if a mandatory parameter is missing.
+     */
+    public static function normalizeSearchPhotosRequest(SearchPhotosRequest $request) {
+
+        $parameters = [];
+
+        if (null === $request->getQuery()) {
+            throw new InvalidArgumentException("The mandatory parameter \"query\" is missing");
+        }
+
+        ArrayHelper::set($parameters, "query", $request->getQuery());
+        ArrayHelper::set($parameters, "per_page", $request->getPerPage(), [null]);
+        ArrayHelper::set($parameters, "page", $request->getPage(), [null]);
+
+        return $parameters;
+    }
+
+    /**
+     * Normalize a search videos request.
+     *
+     * @param SearchVideosRequest $request The search photos request.
+     * @return array Returns the parameters.
+     * @throws InvalidArgumentException Throws an invalid argument exception if a mandatory parameter is missing.
+     */
+    public static function normalizeSearchVideosRequest(SearchVideosRequest $request) {
+
+        $parameters = [];
+
+        if (null === $request->getQuery()) {
+            throw new InvalidArgumentException("The mandatory parameter \"query\" is missing");
+        }
+
+        ArrayHelper::set($parameters, "query", $request->getQuery());
+        ArrayHelper::set($parameters, "per_page", $request->getPerPage(), [null]);
+        ArrayHelper::set($parameters, "page", $request->getPage(), [null]);
+        ArrayHelper::set($parameters, "min_width", $request->getMinWidth(), [null]);
+        ArrayHelper::set($parameters, "max_width", $request->getMaxWidth(), [null]);
+        ArrayHelper::set($parameters, "min_duration", $request->getMinDuration(), [null]);
+        ArrayHelper::set($parameters, "max_duration", $request->getMaxDuration(), [null]);
+
+        return $parameters;
+    }
+}
---
+++ tests/Normalizer/RequestNormalizerTest.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Tests\Normalizer;
+
+use Exception;
+use InvalidArgumentException;
+use WBW\Library\Pexels\Model\Request\CuratedPhotosRequest;
+use WBW\Library\Pexels\Model\Request\PopularVideosRequest;
+use WBW\Library\Pexels\Model\Request\SearchPhotosRequest;
+use WBW\Library\Pexels\Model\Request\SearchVideosRequest;
+use WBW\Library\Pexels\Normalizer\RequestNormalizer;
+use WBW\Library\Pexels\Tests\AbstractTestCase;
+
+/**
+ * Request normalizer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Tests\Normalizer
+ */
+class RequestNormalizerTest extends AbstractTestCase {
+
+    /**
+     * Tests the normalizeCuratedPhotosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizeCuratedPhotosRequest() {
+
+        // Set a Curated photos request mock.
+        $curatedPhotosRequest = new CuratedPhotosRequest();
+
+        $res = RequestNormalizer::normalizeCuratedPhotosRequest($curatedPhotosRequest);
+        $this->assertEquals(15, $res["per_page"]);
+        $this->assertEquals(1, $res["page"]);
+    }
+
+    /**
+     * Tests the normalizePopularVideosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizePopularVideosRequest() {
+
+        // Set a Popular videos request mock.
+        $popularVideosRequest = new PopularVideosRequest();
+        $popularVideosRequest->setMinWidth(1280);
+        $popularVideosRequest->setMaxWidth(1920);
+        $popularVideosRequest->setMinDuration(1);
+        $popularVideosRequest->setMaxDuration(60);
+
+        $res = RequestNormalizer::normalizePopularVideosRequest($popularVideosRequest);
+        $this->assertEquals(15, $res["per_page"]);
+        $this->assertEquals(1, $res["page"]);
+        $this->assertEquals(1280, $res["min_width"]);
+        $this->assertEquals(1920, $res["max_width"]);
+        $this->assertEquals(1, $res["min_duration"]);
+        $this->assertEquals(60, $res["max_duration"]);
+    }
+
+    /**
+     * Tests the normalizeSearchPhotosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizeSearchPhotosRequest() {
+
+        // Set a Search photos request mock.
+        $searchPhotosRequest = new SearchPhotosRequest();
+        $searchPhotosRequest->setQuery("github");
+
+        $res = RequestNormalizer::normalizeSearchPhotosRequest($searchPhotosRequest);
+        $this->assertEquals("github", $res["query"]);
+        $this->assertEquals(15, $res["per_page"]);
+        $this->assertEquals(1, $res["page"]);
+    }
+
+    /**
+     * Tests the normalizeSearchPhotosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizeSearchPhotosRequestWithInvalidArgumentException() {
+
+        // Set a Search photos request mock.
+        $searchPhotosRequest = new SearchPhotosRequest();
+
+        try {
+
+            RequestNormalizer::normalizeSearchPhotosRequest($searchPhotosRequest);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals("The mandatory parameter \"query\" is missing", $ex->getMessage());
+        }
+    }
+
+    /**
+     * Tests the normalizeSearchVideosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizeSearchVideosRequest() {
+
+        // Set a Search videos request mock.
+        $searchVideosRequest = new SearchVideosRequest();
+        $searchVideosRequest->setQuery("github");
+        $searchVideosRequest->setMinWidth(1280);
+        $searchVideosRequest->setMaxWidth(1920);
+        $searchVideosRequest->setMinDuration(1);
+        $searchVideosRequest->setMaxDuration(60);
+
+        $res = RequestNormalizer::normalizeSearchVideosRequest($searchVideosRequest);
+        $this->assertEquals("github", $res["query"]);
+        $this->assertEquals(15, $res["per_page"]);
+        $this->assertEquals(1, $res["page"]);
+        $this->assertEquals(1280, $res["min_width"]);
+        $this->assertEquals(1920, $res["max_width"]);
+        $this->assertEquals(1, $res["min_duration"]);
+        $this->assertEquals(60, $res["max_duration"]);
+    }
+
+    /**
+     * Tests the normalizeSearchVideosRequest() method.
+     *
+     * @return void
+     */
+    public function testNormalizeSearchVideosRequestWithInvalidArgumentException() {
+
+        // Set a Search videos request mock.
+        $searchPhotosRequest = new SearchVideosRequest();
+
+        try {
+
+            RequestNormalizer::normalizeSearchVideosRequest($searchPhotosRequest);
+        } catch (Exception $ex) {
+
+            $this->assertInstanceOf(InvalidArgumentException::class, $ex);
+            $this->assertEquals("The mandatory parameter \"query\" is missing", $ex->getMessage());
+        }
+    }
+}
