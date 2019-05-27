826d589b905c0bf0508e522a7795164117917755
webeweb <support@webeweb.fr>
2019-05-27 18:15:51 +0200

    Add Per page trait

---
+++ src/Traits/PerPageTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Traits;
+
+/**
+ * Page trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Traits
+ */
+trait PerPageTrait {
+
+    /**
+     * Per page.
+     *
+     * @var int
+     */
+    private $perPage;
+
+    /**
+     * Get the per page.
+     *
+     * @return int Returns the per page.
+     */
+    public function getPerPage() {
+        return $this->perPage;
+    }
+
+    /**
+     * Set the per page.
+     *
+     * @param int $perPage The per page.
+     */
+    public function setPerPage($perPage) {
+        $this->perPage = $perPage;
+        return $this;
+    }
+}
