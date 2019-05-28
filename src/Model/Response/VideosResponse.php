54f76455e6c93928f11af2c8e2b6d14845bd1ff8
webeweb <support@webeweb.fr>
2019-05-28 18:57:19 +0200

    Add Videos response

---
+++ src/Model/Response/VideosResponse.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Model\Response;
+
+use WBW\Library\Pexels\Model\AbstractResponse;
+use WBW\Library\Pexels\Model\Video;
+use WBW\Library\Pexels\Traits\NextPageTrait;
+use WBW\Library\Pexels\Traits\PageTrait;
+use WBW\Library\Pexels\Traits\PerPageTrait;
+use WBW\Library\Pexels\Traits\PrevPageTrait;
+use WBW\Library\Pexels\Traits\TotalResultsTrait;
+use WBW\Library\Pexels\Traits\UrlTrait;
+
+/**
+ * Videos response.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Model\Response
+ */
+class VideosResponse extends AbstractResponse {
+
+    use NextPageTrait;
+    use PageTrait;
+    use PerPageTrait;
+    use PrevPageTrait;
+    use TotalResultsTrait;
+    use UrlTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Add a video.
+     *
+     * @param Video $video The video.
+     * @return VideosResponse Returns this video response.
+     */
+    public function addVideo(Video $video) {
+        return $this->addMedia($video);
+    }
+
+    /**
+     * Get the videos.
+     *
+     * @return Video[] Returns the videos.
+     */
+    public function getVideos() {
+        return $this->getMedias();
+    }
+}
---
+++ tests/Model/Response/VideosResponseTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Tests\Model\Response;
+
+use WBW\Library\Pexels\Model\Video;
+use WBW\Library\Pexels\Model\Response\VideosResponse;
+use WBW\Library\Pexels\Tests\AbstractTestCase;
+
+/**
+ * Videos response test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Tests\Model\Response
+ */
+class VideosResponseTest extends AbstractTestCase {
+
+    /**
+     * Tests the addVideo() method.
+     *
+     * @return void
+     */
+    public function testAddVideo() {
+
+        // Set a Video mock.
+        $video = new Video();
+
+        $obj = new VideosResponse();
+
+        $obj->addVideo($video);
+        $this->assertSame($video, $obj->getVideos()[0]);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new VideosResponse();
+
+        $this->assertNull($obj->getNextPage());
+        $this->assertNull($obj->getPage());
+        $this->assertNull($obj->getPerPage());
+        $this->assertCount(0, $obj->getVideos());
+        $this->assertNull($obj->getPrevPage());
+        $this->assertNull($obj->getTotalResults());
+        $this->assertNull($obj->getUrl());
+    }
+}
