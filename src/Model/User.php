4bb32375dba41bf05afe0e61948d4866b956e71d
webeweb <support@webeweb.fr>
2020-10-28 19:23:17 +0100

    Add User model

---
+++ src/Model/User.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Model;
+
+use WBW\Library\Core\Model\Attribute\IntegerIdTrait;
+use WBW\Library\Core\Model\Attribute\StringNameTrait;
+use WBW\Library\Core\Model\Attribute\StringUrlTrait;
+
+/**
+ * User.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Model
+ */
+class User {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use StringNameTrait;
+    use StringUrlTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+}
---
+++ tests/Model/UserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the pexels-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pexels\Tests\Model;
+
+use WBW\Library\Pexels\Model\User;
+use WBW\Library\Pexels\Tests\AbstractTestCase;
+
+/**
+ * User test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pexels\Tests\Model
+ */
+class UserTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new User();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getName());
+        $this->assertNull($obj->getUrl());
+    }
+}
