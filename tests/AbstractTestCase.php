1309c1d1e78fe58da98c84f7b48f496f62683476
webeweb <support@webeweb.fr>
2023-02-20 09:45:23 +0100

    Code checkstyle

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -23,18 +23,17 @@ use PHPUnit\Framework\TestCase;
 abstract class AbstractTestCase extends TestCase {
 
     /**
-     * Get the token.
+     * Get a token.
      *
      * @return string Returns the token.
      */
     public static function getToken(): string {
 
         $path = __DIR__ . "/../.token";
-
         if (true === file_exists($path)) {
             return file_get_contents($path);
         }
 
-        return "YOUR_API_KEY";
+        return "API_TOKEN_MOCK";
     }
 }
