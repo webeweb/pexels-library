57d3a66eb44d1157acbe4ef3bee145acf5a70204
webeweb <support@webeweb.fr>
2024-05-28 08:42:07 +0200

    Code checkstyle

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 /*
  * This file is part of the pexels-library package.
  *
@@ -10,6 +8,9 @@ declare(strict_types = 1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types = 1);
+
 if (true === file_exists($file = __DIR__ . "/autoload.php")) {
     require_once $file;
 } else if (true === file_exists($file = __DIR__ . "/autoload.php.dist")) {
